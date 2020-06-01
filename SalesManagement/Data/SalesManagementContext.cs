88a7641288027fd53349f7a173bb1e275ccf20e9
Guilherme Beneti <guihermebeneti@icloud.com>
2020-06-01 11:53:15 -0300

    Department Scaffolding

---
+++ SalesManagement/Data/SalesManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesManagement.Models;
+
+namespace SalesManagement.Data
+{
+    public class SalesManagementContext : DbContext
+    {
+        public SalesManagementContext (DbContextOptions<SalesManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesManagement.Models.Department> Department { get; set; }
+    }
+}
--- SalesManagement/Startup.cs
+++ SalesManagement/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesManagement.Data;
 
 namespace SalesManagement
 {
@@ -33,6 +35,9 @@ namespace SalesManagement
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesManagementContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesManagementContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
