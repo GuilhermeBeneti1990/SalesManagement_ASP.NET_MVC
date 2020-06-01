b32716c8c1eb6354bd922f1790c0efef9171939b
Guilherme Beneti <guihermebeneti@icloud.com>
2020-06-01 14:21:41 -0300

    Seller form created

--- SalesManagement/Controllers/SellersController.cs
+++ SalesManagement/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesManagement.Models;
 using SalesManagement.Services;
 
 namespace SalesManagement.Controllers
@@ -21,6 +22,19 @@ namespace SalesManagement.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
