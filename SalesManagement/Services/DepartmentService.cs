c04a4bd11d4123a375f0c4a548228402e9aaf3bc
Guilherme Beneti <guihermebeneti@icloud.com>
2020-06-02 09:54:53 -0300

    Async methods implemented

--- SalesManagement/Services/DepartmentService.cs
+++ SalesManagement/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesManagement.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesManagement.Data;
 using SalesManagement.Models;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,9 @@ namespace SalesManagement.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
